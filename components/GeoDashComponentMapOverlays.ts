aeb8bc43dc7380412c666144d64fc31c2abe76c6
pjdufour <pjdufour.dev@gmail.com>
2017-10-10 23:26:35 -0400

    improvement in templating

--- components/GeoDashComponentMapOverlays.ts
+++ components/GeoDashComponentMapOverlays.ts
@@ -2,17 +2,23 @@ declare var extract: any;
 declare var geodash: any;
 declare var $: any;
 
-/* Components */
-import { Component, OnInit, EventEmitter, ElementRef } from '@angular/core';
+/* Core */
+import { Component, OnInit, EventEmitter, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 /* Services */
 import { GeoDashServiceBus }  from 'GeoDashServiceBus';
 import { GeoDashServiceCompile } from 'GeoDashServiceCompile';
 
+/* Pipes */
+import { GeoDashPipeSlugify }  from 'GeoDashPipeSlugify';
+
 @Component({
   selector: 'geodash-map-overlays',
   template: geodash.api.getTemplate('geodashMapOverlays.tpl.html'),
-  providers: []
+  providers: [
+    GeoDashPipeSlugify
+  ]
 })
 export class GeoDashComponentMapOverlays implements OnInit {
 
@@ -22,8 +28,9 @@ export class GeoDashComponentMapOverlays implements OnInit {
 
   name = 'GeoDashComponentMapOverlays';
 
-  constructor(private element: ElementRef, private bus: GeoDashServiceBus, private compileService: GeoDashServiceCompile) {
+  constructor(private element: ElementRef, private changeDetector: ChangeDetectorRef, private sanitizer: DomSanitizer, private bus: GeoDashServiceBus, private compileService: GeoDashServiceCompile, private slugify: GeoDashPipeSlugify) {
 
+    this.dashboard = undefined;
     this.state = {};
     this.overlays = [];
   }
@@ -40,28 +47,83 @@ export class GeoDashComponentMapOverlays implements OnInit {
     this.dashboard = data["dashboard"];
     this.state = data["state"];
 
-    this.refreshOverlays() // sets this.overlays
+    this.overlays = extract("overlays", this.dashboard, []).map((overlay: any): any => geodash.util.extend(overlay, <any>{
+        "classes": this.class_overlay(overlay),
+        "style":  this.style_overlay(overlay, this.state),
+        "intents":  this.build_intents(overlay),
+        "src": this.imageURL(overlay),
+        "link_url_template": extract("link.url", overlay),
+        "link_target": extract("link.target", overlay),
+        "link": this.build_link(
+          overlay,
+          extract("link.url", overlay),
+          extract("link.target", overlay),
+          this.dashboard,
+          this.state
+        ),
+        "text_content_template": extract("text.content", overlay),
+        "text_font": extract("text.font", overlay),
+        "text_content": this.build_text(extract("text.content", overlay), this.dashboard, this.state)
+    }));
 
     console.log("overlays =", this.overlays);
-    setTimeout(() => {
-      $('[data-toggle="tooltip"]', this.element.nativeElement).tooltip();
-    },0);
+    setTimeout((function(element){
+      return function() {
+        $('[data-toggle="tooltip"]', element).tooltip();
+      };
+    })(this.element.nativeElement), 0);
   }
 
   onRefresh = (name: any, data: any, source: any): void => {
     this.state = data["state"];
-    this.refreshOverlays() // sets this.overlays
+    var changed = false;
+    for(var i = 0; i < this.overlays.length; i++)
+    {
+      var before_style = JSON.stringify(this.overlays[i]['style']);
+      var before_link = JSON.stringify(this.overlays[i]['link']);
+      var before_text = JSON.stringify(this.overlays[i]['text_content']);
+      this.overlays[i]['style'] = this.style_overlay(this.overlays[i], this.state);
+      this.overlays[i]['link'] = this.build_link(
+        this.overlays[i],
+        extract("link_url_template", this.overlays[i]),
+        extract("link_template", this.overlays[i]),
+        this.dashboard,
+        this.state);
+      var text_content_template = extract("text_content_template", this.overlays[i]);
+      if(geodash.util.isDefined(text_content_template))
+      {
+        this.overlays[i]["text_content"] = this.build_text(
+          text_content_template,
+          this.dashboard,
+          this.state);
+      }
+      if(
+        before_style != JSON.stringify(this.overlays[i]['style']) ||
+        before_link != JSON.stringify(this.overlays[i]['link']) ||
+        before_text != JSON.stringify(this.overlays[i]['text_content'])
+      )
+      {
+        changed = true;
+      }
+    }
+    if(changed)
+    {
+      this.changeDetector.detectChanges();
+    }
   }
 
-  onClick = (event: any, overlay: any): void => {
+  onClickOverlay = (event: any, overlay: any): void => {
     var link = extract("link", overlay);
     if(! geodash.util.isDefined(link))
     {
       var intents = extract("intents", overlay, []);
-      intents.forEach((intent:any) => {
-        let data = this.render(intent.data, <any>{"overlay": overlay});
-        this.bus.emit("intents", intent.name, data, this.name);
-      });
+      if(geodash.util.isDefined(intents))
+      {
+        intents.forEach((intent:any) => {
+          var intentData = this.render(intent.data, <any>{"overlay": overlay});
+          this.bus.emit("intents", intent.name, intentData, this.name);
+        });
+      }
       event.preventDefault();
     }
   }
@@ -77,17 +139,6 @@ export class GeoDashComponentMapOverlays implements OnInit {
 
   interpolate = (template: string): any => {
       return (ctx:any) => this.compileService.compile(template, ctx);
-  }
-
-  refreshOverlays = (): void => {
-
-    this.overlays = extract("overlays", this.dashboard, []).map((overlay: any): any => geodash.util.extend(overlay, <any>{
-        "classes": this.class_overlay(overlay),
-        "style":  this.style_overlay(overlay.type, overlay),
-        "intents":  this.intents(overlay),
-        "src": this.imageURL(overlay)
-    }));
-
   }
 
   imageURL(overlay: any): string {
@@ -128,7 +179,7 @@ export class GeoDashComponentMapOverlays implements OnInit {
     return str;
   };
 
-  style_overlay(type: string, overlay: any): any {
+  style_overlay(overlay: any, state: any): any {
     var styleMap = <any>{};
 
     geodash.util.extend(styleMap, <any>{
@@ -144,7 +195,7 @@ export class GeoDashComponentMapOverlays implements OnInit {
       "opacity": "1.0"
     });
 
-    if(type == "text")
+    if(overlay.type == "text")
     {
       geodash.util.extend(styleMap, <any>{
         "font-family": extract("text.font.family", overlay, 'Arial'),
@@ -153,7 +204,7 @@ export class GeoDashComponentMapOverlays implements OnInit {
         "text-shadow": extract("text.shadow", overlay, 'none')
       });
     }
-    else if(type == "image")
+    else if(overlay.type == "image")
     {
       geodash.util.extend(styleMap, <any>{
         "display": "inline-block"
@@ -172,10 +223,22 @@ export class GeoDashComponentMapOverlays implements OnInit {
       geodash.util.extend(styleMap, geodash.util.arrayToObject(extract("css.properties", overlay)));
     }
 
+    if(geodash.util.isDefined(state))
+    {
+      if(extract("view.overlays", state, []).indexOf(overlay.id) == -1)
+      {
+        styleMap["display"] = "none";
+      }
+    }
+    else
+    {
+      styleMap["display"] = "none";
+    }
+
     return styleMap;
   };
 
-  intents(overlay: any): any {
+  build_intents(overlay: any): any {
     var data = [];
     var intents = extract("intents", overlay);
     if(Array.isArray(intents))
@@ -200,6 +263,47 @@ export class GeoDashComponentMapOverlays implements OnInit {
       }
     }
     return data;
+  };
+
+  build_link(overlay: any, link_url_template: any, link_target: any, dashboard: any, state: any): any {
+    if(geodash.util.isDefined(link_url_template))
+    {
+      var link_url: SafeResourceUrl = undefined;
+      var link_url_string = this.interpolate(link_url_template)({"dashboard": dashboard, "state": state});
+      if(
+        link_url_string.startsWith("http://") ||
+        link_url_string.startsWith("https://") ||
+        link_url_string.startsWith("sms:") ||
+        link_url_string.startsWith("tel:") ||
+        link_url_string.startsWith("mailto:")
+      )
+      {
+        link_url = this.sanitizer.bypassSecurityTrustResourceUrl(link_url_string);
+      }
+      else
+      {
+        link_url = this.sanitizer.bypassSecurityTrustResourceUrl("http://"+link_url_string);
+      }
+      return {
+        "url": link_url,
+        "target": link_target || "_blank"
+      }
+    }
+    else
+    {
+      return undefined;
+    }
+  };
+
+  build_text(text_content_template: any, dashboard: any, state: any): any {
+    if(geodash.util.isDefined(text_content_template))
+    {
+      return this.interpolate(text_content_template)({"dashboard": dashboard, "state": state});
+    }
+    else
+    {
+      return undefined;
+    }
   };
 
 }
